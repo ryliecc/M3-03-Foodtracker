d66115e4be84a8bf91447825d27f6be41dc60d08
Rylie Castell <rylie.castell@gmail.com>
2025-05-27 16:53:16 +0200

    remove spacing

--- FoodTracker/ContentView.swift
+++ FoodTracker/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HeaderView()
-        EntryListView()
+        VStack(spacing: 0) {
+            HeaderView()
+            EntryListView()
+        }
     }
 }
 
