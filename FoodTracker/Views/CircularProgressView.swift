d792340ca9e00413be3831f804d684007513996e
Rylie Castell <rylie.castell@gmail.com>
2025-05-30 20:57:19 +0200

    add visible overlap for more than 100 percent calorie goal

--- FoodTracker/Views/CircularProgressView.swift
+++ FoodTracker/Views/CircularProgressView.swift
@@ -13,25 +13,49 @@ struct CircularProgressView: View {
     var body: some View {
         ZStack {
             Circle()
+                .stroke(Color(.white), lineWidth: 20)
+                .frame(width: 250, height: 250)
+            Circle()
                 .stroke(Color("SecondaryColor"), lineWidth: 20)
+                .frame(width: 220, height: 220)
             Circle()
                 .trim(from: 0, to: progress)
-                .stroke(
-                    (progress <= 1 ? Color("PrimaryColor") : Color(.red)),
+                .stroke(Color("PrimaryColor"),
                     style: StrokeStyle(lineWidth: 20, lineCap: .round)
                 )
                 .rotationEffect(.degrees(-90))
+                .frame(width: 220, height: 220)
+            if progress > 1.0 {
+                            Circle()
+                                .trim(from: 0, to: progress - 1.0)
+                                .stroke(Color("PrimaryColor"),
+                                        style: StrokeStyle(lineWidth: 20, lineCap: .round))
+                                .rotationEffect(.degrees(-90 + 360))
+                                .frame(width: 220, height: 220)
+                            Circle()
+                                .fill(Color("PrimaryColor"))
+                                .frame(width: 20, height: 20)
+                                .shadow(color: Color(.black), radius: 8, x: 14, y: 0)
+                                .offset(x: 0, y: -110)
+                                .rotationEffect(.degrees((progress - 1.0) * 360))
+                                .frame(width: 220, height: 220)
+                        }
+            Circle()
+                .stroke(Color(.white), lineWidth: 16)
+            Circle()
+                .fill(.white)
+                .frame(width: 200)
             Text(centerText)
                 .font(Fonts.dashboardCalories)
                 .multilineTextAlignment(.center)
                 .foregroundColor(Color("PrimaryColor"))
         }
-        .frame(width: 220, height: 220)
+        .frame(width: 256, height: 256)
     }
 }
 
 #Preview {
-    var progress = 0.75
+    var progress = 1.1
     var text = "1.043\nkcal"
     CircularProgressView(progress: progress, centerText: text)
 }
